700ba00ab683a813a126b336825fdac51cf20d81
BrunoAmezcua <amezcua_bruno@hotmail.com>
2019-11-15 11:22:32 -0600

    Raised Button

---
+++ lietemplate/android/app/src/main/kotlin/com/example/lietemplate/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.lietemplate
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
